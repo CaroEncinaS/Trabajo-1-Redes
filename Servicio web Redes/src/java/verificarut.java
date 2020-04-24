2a45cbf997b8a94dea6658111ef8f37003dfcb80
Carolina Encina Sandoval <carolina.encinas@utem.cl>
2020-04-24 13:03:04 -0400

    implementación rut válido

--- Servicio web Redes/src/java/verificarut.java
+++ Servicio web Redes/src/java/verificarut.java
@@ -16,14 +16,29 @@ import javax.ws.rs.QueryParam;
 
 public class verificarut {
     @GET
-    public String verificar(@QueryParam("rut") int Rut) { 
-       String nuevoRut = Integer.toString(Rut); //Pasa el rut a un string
+    public String verificar(@QueryParam("rut") String Rut) {
+       int intRut;
+       String newRut="";
+       String newDv="";
+       for(int i =0, check =0;i<Rut.length() ;i++){
+           if(Rut.charAt(i)=='-'){
+               check=1;
+           }
+           else if(check==0){
+               newRut+=Rut.charAt(i);
+           }
+           else if(check==1){
+               newDv+=Rut.charAt(i);
+           }
+       }
+       intRut = Integer.parseInt(newRut);
+       String nuevoRut = newRut; //Pasa el rut a un string
        int largoCadena = nuevoRut.length(); // devuelve el largo del rut
        /** Crear un arreglo */
        int[] arregloRut = new int[largoCadena];
        for (int i = 0; i < nuevoRut.length(); i++) { 
-        arregloRut[i] = Rut%10;
-        Rut = Rut/10;
+        arregloRut[i] = intRut%10;
+        intRut = intRut/10;
     }
        for (int i = 0, j=2; i < nuevoRut.length(); i++, j++) {
            if(j==8){
@@ -43,11 +58,20 @@ public class verificarut {
            dv = "0";
        }
        else if(digitoverificador == 10) {
-           dv = "K";
+           dv = "k";
        }
        else{
            dv = Integer.toString(digitoverificador);
        }
-       return dv;
+       newDv=newDv.toLowerCase();
+       if(dv.equals(newDv)){
+           return "true";
+       }
+       else{
+           return "false";
+       }
     }
 }
+
+
+
