61338805497b3882c5b15b089b17557d768442c3
Bruno Garrido <bruno.garridoj@utem.cl>
2020-05-03 20:56:50 -0400

    Validación y cambios en el formulario
    Se validó el ingreso del ingreso del rut y además se modificó la lógica del formulario.

--- Cliente Rest/ClienteRest/core/views.py
+++ Cliente Rest/ClienteRest/core/views.py
@@ -5,15 +5,33 @@ import json
 # Create your views here.
 
 def home(request):
-    informacion=request.POST.get('rut')
-    rut=str(informacion)
-    response= requests.get('http://localhost:8080/mavenproject1/app/verificarut?rut='+rut)
-    if response.status_code==200:
-        data= response.json()
-        print(data[0]['valido'])
-        print(response)
-        return render(request, "core/home.html",{
-            'rutvalido':data[0]['valido'],
-        })
+    if request.POST.get('rut'):
+        informacion=request.POST.get('rut')
+        rut=str(informacion)
+        response= requests.get('http://localhost:8080/ProyectoRedes/app/verificarut?rut='+rut)
+        if response.status_code==200:
+            data= response.json()
+            print(data[0]['valido'])
+            print(response)
+            return render(request, "core/home.html",{
+                'rutvalido':data[0]['valido'],
+            })
+        else:
+            return render(request, "core/home.html")
+    elif request.POST.get('nombres'):
+        nombres=str(request.POST.get('nombres'))
+        apellidopat=str(request.POST.get('ApellidoP'))
+        apellidomat=str(request.POST.get('ApellidoM'))
+        sexo=str(request.POST.get('sexo'))
+        response= requests.get('http://localhost:8080/mavenproject1/app/verificarut?rut='+rut)
+        if response.status_code==200:
+            data= response.json()
+            print(data[0]['valido'])
+            print(response)
+            return render(request, "core/home.html",{
+                'rutvalido':data[0]['valido'],
+            })
+        else:
+            return render(request, "core/home.html")
     else:
         return render(request, "core/home.html")
